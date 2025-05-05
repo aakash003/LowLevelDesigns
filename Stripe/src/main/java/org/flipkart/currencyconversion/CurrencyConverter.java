6802b9d09f391e7b2290fece19a93bebc6b8ed58
Aakash <kumar.aakash10@gmail.com>
2025-05-05 10:03:48 +0530

    added

---
+++ Stripe/src/main/java/org/flipkart/currencyconversion/CurrencyConverter.java
@@ -0,0 +1,58 @@
+package org.flipkart.currencyconversion;
+
+import java.util.*;
+
+// "static void main" must be defined in a public class.
+public class CurrencyConverter {
+    public static void main(String[] args) {
+        List<Node> data = new ArrayList<Node>();
+        data.add(new Node("USD", "JPY", 110));
+        data.add(new Node("USD", "AUD", 1.45));
+        data.add(new Node("JPY", "GBP", 0.0070));
+        System.out.println(getRatio("GBP", "AUD", data));
+    }
+
+    public static double getRatio(String start, String end, List<Node> data) {
+        Map<String, Map<String, Double>> map = new HashMap();
+        for (Node node : data) {
+            if (!map.containsKey(node.start)) map.put(node.start, new HashMap());
+            map.get(node.start).put(node.end, node.ratio);
+            if (!map.containsKey(node.end)) map.put(node.end, new HashMap());
+            map.get(node.end).put(node.start, 1.0 / node.ratio);
+        }
+        Queue<String> q = new LinkedList();
+        Queue<Double> val = new LinkedList();
+        q.offer(start);
+        val.offer(1.0);
+        Set<String> visited = new HashSet();
+        while (!q.isEmpty()) {
+            String cur = q.poll();
+            double num = val.poll();
+            if (visited.contains(cur)) continue;
+            visited.add(cur);
+            if (map.containsKey(cur)) {
+                Map<String, Double> next = map.get(cur);
+                for (String key : next.keySet()) {
+                    if (!visited.contains(key)) {
+                        q.offer(key);
+                        if (key.equals(end)) return num * next.get(key);
+                        val.offer(num * next.get(key));
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    static class Node {
+        String start;
+        String end;
+        double ratio;
+
+        public Node(String s, String e, double r) {
+            this.start = s;
+            this.end = e;
+            this.ratio = r;
+        }
+    }
+}
---
+++ Stripe/src/main/java/org/flipkart/currencyconversion/readme.md
@@ -0,0 +1,4 @@
+Given a string that has currency "USD:CAD:074,AUD:CAD" as a data, then I was told to write a function would take two
+input currency string and a number(value in first currency) and then determine the value of number in the second
+currency The follow up questions was Given same data above, write a function that would take two currency string, and a
+number and the fine the value of that number
