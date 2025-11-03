cdf11b2cf2d0b5baf84e122d60f0629d673ab441
Aakash <kumar.aakash10@gmail.com>
2025-11-03 15:44:30 +0530

    Update SearchEngine.java

--- Confluent/src/main/java/org/flipkart/SearchEngine.java
+++ Confluent/src/main/java/org/flipkart/SearchEngine.java
@@ -107,9 +107,46 @@ public class SearchEngine {
 }
 
 /**
- * Operation	Time Complexity	Space Complexity	Notes
- * Build Index	O(T)	O(V + T)	One-time cost
- * Search (1 token)	O(k)	—	k = docs containing that token
- * Search (phrase)	O(m * f * d)	—	m = words in phrase
- * Tokenize (search phrase)	O(m)	—	negligible
+ Phase	Complexity	Depends on
+ Build Index	O(N) time, O(V + N) space	(V = unique words, N = total tokens)
+ Search Phrase	O(m + m×avgDocListSize + C×P) time, O(P) space	(m = query length, C = candidate docs, P = total positions)
  */
+
+
+//private boolean isPhrasePresent(int docId, List<String> tokens) {
+//    List<List<Integer>> positions = new ArrayList<>();
+//    for (String token : tokens) {
+//        List<Integer> posList = invertedIndex.get(token).get(docId);
+//        if (posList == null) return false;
+//        positions.add(posList);
+//    }
+//
+//    // Start by aligning all positions with two-pointer merge
+//    List<Integer> base = positions.get(0);
+//    for (int pos : base) {
+//        boolean match = true;
+//        int expected = pos + 1;
+//
+//        for (int i = 1; i < positions.size(); i++) {
+//            List<Integer> curr = positions.get(i);
+//            int j = 0;
+//
+//            // Advance until curr[j] >= expected
+//            while (j < curr.size() && curr.get(j) < expected) j++;
+//
+//            if (j == curr.size() || !curr.get(j).equals(expected)) {
+//                match = false;
+//                break;
+//            }
+//            expected++; // move next expected position
+//        }
+//
+//        if (match) return true;
+//    }
+//
+//    return false;
+//}
+//Maintain a pointer in each sorted list.
+//
+//Compare them and move the “lagging” one forward until they align (or miss).
+
